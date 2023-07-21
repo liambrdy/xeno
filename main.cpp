538ab6046bf9fd71ce6dfa16c4de207fbffb4a13
liambrdy <liam.james.brady@gmail.com>
2023-07-21 13:27:21 -0400

    added chunk system

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #define Megabytes(x) (Kilobytes(x)*1024)
 #define Gigabytes(x) (Megabytes(x)*1024)
 
+#define ArrayCount(arr) (sizeof(arr)/sizeof(arr[0]))
+
 typedef int8_t  i8;
 typedef int16_t i16;
 typedef int32_t i32;
@@ -21,32 +23,23 @@ typedef uint64_t u64;
 typedef float f32;
 typedef double f64;
 
-typedef struct {
+#define CHUNK_UNINITIALIZED INT32_MAX
 
-} Entity;
+struct Chunk {
+    i32 x, y;
 
-typedef struct {
-    int x, y;
-    Entity *entities;
-} Chunk;
+    Chunk *next;
+};
 
-typedef struct WorldChunk {
-    Chunk c;
-    struct WorldChunk *next;
-} WorldChunk;
-
-typedef struct {
+struct MemoryArena {
     u8 *memory;
     size_t capacity;
     size_t index;
-} MemoryArena;
+};
 
-typedef struct {
-    WorldChunk *worldChunks;
-    size_t chunkCount;
-
-    MemoryArena arena;
-} World;
+struct World {
+    Chunk chunks[4096];
+};
 
 void ArenaInit(MemoryArena *arena, void *mem, size_t capacity)
 {
@@ -68,43 +61,50 @@ void ArenaClear(MemoryArena *arena)
     arena->index = 0;
 }
 
-#define PushStruct(arena, type) (type *)ArenaPush(&arena, sizeof(type))
-#define PushArray(arena, type, count) (type *)ArenaPush(&arena, count * sizeof(type))
+#define PushStruct(arena, type) (type *)ArenaPush(arena, sizeof(type))
+#define PushArray(arena, type, count) (type *)ArenaPush(arena, count * sizeof(type))
 
-Tile *GetTile(World *world, int x, int y, TileType type)
+Chunk *GetChunk(World *world, int x, int y, MemoryArena *arena)
 {
     int slotHash = 7 * x + 2 * y;
-    int slotIndex = slotHash & 1024;
-    assert(slotIndex < 1024);
+    int slotIndex = slotHash & (ArrayCount(world->chunks)-1);
+    assert(slotIndex < ArrayCount(world->chunks));
 
-    Tile *result = NULL;
+    Chunk *chunk = &world->chunks[slotIndex];
 
-    WorldTile worldTile = world->worldTiles[slotIndex];
-    if (worldTile.t.x == x && worldTile.t.y == y && worldTile.t.type == type) {
-        result = &worldTile.t;
-    } else {
-        while (worldTile.next) {
-            if (worldTile.t.x == x && worldTile.t.y == y && worldTile.t.type == type) {
-                result = &worldTile.t;
-                break;
-            }
-            worldTile = *worldTile.next;
+    do
+    {
+        if (chunk->x == x && chunk->y == y) {
+            break;
         }
-    }
 
-    if (!result) {
-        Tile t = {};
-        t.x = x;
-        t.y = y;
-        t.type = type;
-        worldTile.next = PushStruct(world->arena, WorldTile);
-        worldTile.next->next = NULL;
-        worldTile.next->t = t;
-        result = &worldTile.next->t;
-        world->tileCount++;
-    }
+        if (chunk->x != CHUNK_UNINITIALIZED && !chunk->next) {
+            chunk->next = PushStruct(arena, Chunk);
+            chunk = chunk->next;
+            chunk->x = CHUNK_UNINITIALIZED;
+        }
 
-    return result;
+        if (chunk->x == CHUNK_UNINITIALIZED) {
+            chunk->x = x;
+            chunk->y = y;
+
+            chunk->next = 0;
+            
+            break;
+        }
+
+        chunk = chunk->next;
+    } while (chunk);
+    
+    return chunk;
+}
+
+void InitializeWorld(World *world)
+{
+    for (u32 chunkIndex = 0; chunkIndex < ArrayCount(world->chunks); chunkIndex++) {
+        world->chunks[chunkIndex].x = CHUNK_UNINITIALIZED;
+        world->chunks[chunkIndex].next = 0;
+    }
 }
 
 int main()
@@ -119,9 +119,8 @@ int main()
     MemoryArena arena = {};
     ArenaInit(&arena, memoryBlock, memoryBlockSize);
 
-    World *world = PushStruct(arena, World); 
-    world->worldTiles = PushArray(arena, WorldTile, 1024);
-    world->tileCount = 0;
+    World *world = PushStruct(&arena, World);
+    InitializeWorld(world);
 
     while (!WindowShouldClose()) {
         PollInputEvents();
